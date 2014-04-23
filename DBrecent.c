835549deef60fe56c5a1e8f868d96025dd9cf06a
Taiki <taiki@rakshata.com>
2014-04-23 18:20:39 +0200

    Add the SQL code to insert/update in the recent DB

--- DBrecent.c
+++ DBrecent.c
@@ -69,7 +69,7 @@ bool checkRecentDBValid(sqlite3 * DB)
 
 bool addRecentEntry(MANGAS_DATA data, bool wasItADL)
 {
-	time_t timestamp = time(NULL);
+	bool output = false;
 	
 	sqlite3 *database = getPtrRecentDB();
 	if(database == NULL)
@@ -77,18 +77,60 @@ bool addRecentEntry(MANGAS_DATA data, bool wasItADL)
 	
 	sqlite3_stmt * request = NULL;
 	
-	if(sqlite3_prepare_v2(database, "", -1, &request, NULL) != SQLITE_OK)
+	if(sqlite3_prepare_v2(database, "BEGIN EXCLUSIVE TRANSACTION", -1, &request, NULL) != SQLITE_OK || sqlite3_step(request) != SQLITE_DONE)
 	{
 		sqlite3_finalize(request);
 		sqlite3_close(database);
 		return false;
 	}
 	
-	sqlite3_bind_int64(request, 1, timestamp);
-	sqlite3_bind_text(request, 2, data.team->URL_depot, -1, SQLITE_STATIC);
-	sqlite3_bind_text(request, 3, data.mangaNameShort, -1, SQLITE_STATIC);
+	sqlite3_finalize(request);
 	
-	return true;
+	//We check if the element exist
+	if(sqlite3_prepare_v2(database, "SELECT count(*) FROM RakHL3IsALie WHERE "DBNAMETOID(RDB_REC_team)" = ?1 AND "DBNAMETOID(RDB_REC_mangaNameShort)" = ?2;", -1, &request, NULL) == SQLITE_OK)
+	{
+		sqlite3_bind_text(request, 1, data.team->URL_depot, -1, SQLITE_STATIC);
+		sqlite3_bind_text(request, 2, data.mangaNameShort, -1, SQLITE_STATIC);
+		if(sqlite3_step(request) == SQLITE_ROW)
+		{
+			uint nbOccurence = sqlite3_column_int(request, 0);
+			sqlite3_finalize(request);
+			
+			//We'll inject the data now
+			time_t timestamp = time(NULL);
+			time_t recentRead = wasItADL ? 0 : timestamp, recentDL = wasItADL ? timestamp : 0;
+			char requestString[100];
+			
+			//We craft the request
+			if(nbOccurence == 0)
+				snprintf(requestString, sizeof(requestString), "INSERT INTO RakHL3IsALie("DBNAMETOID(RDB_REC_lastRead)", "DBNAMETOID(RDB_REC_lastDL)", "DBNAMETOID(RDB_REC_team)", "DBNAMETOID(RDB_REC_mangaNameShort)") values(?1, ?2, ?3, ?4);");
+			else
+				snprintf(requestString, sizeof(requestString), "UPDATE RakHL3IsALie SET `%d` = ?%d WHERE "DBNAMETOID(RDB_REC_team)" = ?3 AND "DBNAMETOID(RDB_REC_mangaNameShort)" = ?4;", wasItADL ? RDB_REC_lastDL : RDB_REC_lastRead, wasItADL ? 2 : 1);
+			
+			//Setup the handler
+			if(sqlite3_prepare_v2(database, requestString, -1, &request, NULL) == SQLITE_OK)
+			{
+				if(!nbOccurence || !wasItADL)
+					sqlite3_bind_int64(request, 1, recentRead);
+				if(!nbOccurence || wasItADL)
+					sqlite3_bind_int64(request, 2, recentDL);
+
+				sqlite3_bind_text(request, 3, data.team->URL_depot, -1, SQLITE_STATIC);
+				sqlite3_bind_text(request, 4, data.mangaNameShort, -1, SQLITE_STATIC);
+
+				output = sqlite3_step(request) == SQLITE_DONE;
+			}
+		}
+	}
+	sqlite3_finalize(request);
+	
+	if(sqlite3_prepare_v2(database, "END TRANSACTION", -1, &request, NULL) == SQLITE_OK)
+		sqlite3_step(request);
+
+	sqlite3_finalize(request);
+	sqlite3_close(database);
+
+	return output;
 }
 
 MANGAS_DATA ** getRecentEntries (bool wantDL, uint8_t * nbElem)
@@ -113,7 +155,7 @@ MANGAS_DATA ** getRecentEntries (bool wantDL, uint8_t * nbElem)
 	char requestString[96];
 	int code = wantDL ? RDB_REC_lastDL : RDB_REC_lastRead;
 	
-	snprintf(requestString, sizeof(requestString), "SELECT * FROM `RakHL3IsALie` WHERE `%d` <> 0 ORDER BY `%d` ASC", code, code);
+	snprintf(requestString, sizeof(requestString), "SELECT * FROM `RakHL3IsALie` WHERE `%d` > 0 ORDER BY `%d` ASC", code, code);
 	
 	if(sqlite3_prepare_v2(database, requestString, -1, &request, NULL) != SQLITE_OK)
 	{
@@ -126,8 +168,8 @@ MANGAS_DATA ** getRecentEntries (bool wantDL, uint8_t * nbElem)
 	char *team, *mangaName;
 	while (*nbElem < 3 && sqlite3_step(request) == SQLITE_ROW)
 	{
-		team =		(char *) sqlite3_column_text(request, 1);
-		mangaName = (char *) sqlite3_column_text(request, 2);
+		team =		(char *) sqlite3_column_text(request, 2);
+		mangaName = (char *) sqlite3_column_text(request, 3);
 		
 		if(team != NULL && mangaName != NULL)
 		{
