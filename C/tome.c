68a4be4196e7a6377ec9096917d5dc122ae7d872
Taiki <taiki@rakshata.com>
2014-05-18 23:59:13 +0200

    Fix a bug, even if the scenario in which it could be triggered weren’t deeply investigated

--- C/tome.c
+++ C/tome.c
@@ -137,6 +137,9 @@ bool checkTomeReadable(MANGAS_DATA mangaDB, int ID)
 	CONTENT_TOME * cache = mangaDB.tomes[pos].details;
 	char basePath[2*LONGUEUR_NOM_MANGA_MAX + 50], intermediaryDirectory[300], fullPath[2*LONGUEUR_NOM_MANGA_MAX + 350];
 	
+	if (cache == NULL)
+		return false;
+	
 	snprintf(basePath, sizeof(basePath), "manga/%s/%s/", mangaDB.team->teamLong, mangaDB.mangaName);
 	
 	for(posDetails = 0; cache[posDetails].ID != VALEUR_FIN_STRUCTURE_CHAPITRE; posDetails++)
@@ -155,6 +158,14 @@ bool checkTomeReadable(MANGAS_DATA mangaDB, int ID)
 					snprintf(intermediaryDirectory, sizeof(intermediaryDirectory), "Tome_%d/native/Chapitre_%d.%d", ID, cache[posDetails].ID / 10, cache[posDetails].ID % 10);
 				else
 					snprintf(intermediaryDirectory, sizeof(intermediaryDirectory), "Tome_%d/native/Chapitre_%d", ID, cache[posDetails].ID / 10);
+			}
+			else
+			{
+				snprintf(fullPath, sizeof(fullPath), "%s/%s/shared", basePath, intermediaryDirectory);
+				if(!checkFileExist(fullPath))
+				{
+					MDL_createSharedFile(mangaDB, cache[posDetails].ID, ID);
+				}
 			}
 		}
 		else
