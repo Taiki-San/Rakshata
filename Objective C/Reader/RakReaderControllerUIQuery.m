8bbc846c689c94f376fdefe71cc859b42a8501f7
Taiki <taiki@rakshata.com>
2014-06-20 00:08:48 +0200

    Inject INPopoverController Framework to help manipulate NSPopover, may go away if unable to do the job

--- Objective C/Reader/RakReaderControllerUIQuery.m
+++ Objective C/Reader/RakReaderControllerUIQuery.m
@@ -10,42 +10,6 @@
  **                                                                                         **
  ********************************************************************************************/
 
-@class RakReaderBottomBar;
-@class RakPage;
-
-@interface Reader : RakTabView
-{
-	bool initialized;
-	
-	uint gonnaReduceTabs;
-	RakReaderBottomBar * bottomBar;
-	RakPage * mainImage;
-}
-
-- (id)init : (NSView*)contentView : (NSString *) state;
-- (void) initReaderMainView : (NSString *) state;
-
-- (void) startReading : (MANGAS_DATA) project : (int) elemToRead : (bool) isTome : (uint) startPage;
-- (void) willLeaveReader;
-- (void) willOpenReader;
-
-- (void) collapseAllTabs : (bool) forced;
-- (void) hideBothTab;
-- (void) unhideBothTab;
-- (void) hideCursor;
-
-- (void) prevPage;
-- (void) jumpPage : (uint) newPage;
-- (void) nextPage;
-- (void) prevChapter;
-- (void) nextChapter;
-- (void) triggerFullscreen;
-- (void) deleteElement;
-- (void) updatePage : (uint) newCurrentPage : (uint) newPageMax;
+@implementation RakReaderControllerUIQuery
 
 @end
-
-#import "RakReaderControllerUIQuery.h"
-#import "RakPageCounter.h"
-#import "RakReaderBottomBar.h"
-#import "RakPage.h"
