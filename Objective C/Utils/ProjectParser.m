a5c7adbb1a5ae5c6254b0f059edcb0265f3df415
Taiki <taiki@rakshata.com>
2015-03-26 19:36:32 +0100

    This value is used as a canary to detect invalid allocation. Only a problem in debugger but can cause various cryptic crashes

--- Objective C/Utils/ProjectParser.m
+++ Objective C/Utils/ProjectParser.m
@@ -88,7 +88,15 @@ int * getChapters(NSArray * chapterBloc, uint * nbElem, BOOL paidContent, uint *
 						if([entry3 superclass] != [NSNumber class])
 							continue;
 						
-						output[pos++] = [entry3 integerValue];
+						output[pos] = [entry3 integerValue];
+
+#ifdef DEV_VERSION
+						if(output[pos++] == 0xdeadbead)
+						{
+							logR("Error: this value (-559038803) is forbiden, moved by one");
+							output[pos - 1]--;
+						}
+#endif
 					}
 					
 					output[pos] = VALEUR_FIN_STRUCT;
