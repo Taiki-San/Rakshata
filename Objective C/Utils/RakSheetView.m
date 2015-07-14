3870824588483a162fe5211f0071658666c12b91
Taiki <taiki@rakshata.com>
2015-07-14 18:14:46 +0200

    Tweak the UI of RakSheetView to add a border

--- Objective C/Utils/RakSheetView.m
+++ Objective C/Utils/RakSheetView.m
@@ -10,27 +10,40 @@
  **                                                                                         **
  ********************************************************************************************/
 
+#define BORDER_OFFSET 0.5f
+
 @implementation RakSheetView
 
 - (void) drawRect:(NSRect)dirtyRect
 {
 	[_backgroundColor setFill];
+	[[[NSColor whiteColor] colorWithAlphaComponent:0.175] setStroke];
 
 	CGContextRef contextBorder = [[NSGraphicsContext currentContext] graphicsPort];
 
 	NSSize currentSize = _bounds.size;
 	const CGFloat radius = 2;
 
+	//Background
 	CGContextBeginPath(contextBorder);
 	CGContextMoveToPoint(contextBorder, 0, currentSize.height);
 	CGContextAddLineToPoint(contextBorder, currentSize.width, currentSize.height);
 	CGContextAddLineToPoint(contextBorder, currentSize.width, radius);
 	CGContextAddArc(contextBorder, currentSize.width - radius, radius, radius, 0, -M_PI_2, 1);
 	CGContextAddLineToPoint(contextBorder, radius, 0);
-	CGContextAddArc(contextBorder, radius, radius, radius, M_PI_2, M_PI, 1);
+	CGContextAddArc(contextBorder, radius, radius, radius, -M_PI_2, -M_PI, 1);
 	CGContextAddLineToPoint(contextBorder, 0, currentSize.height);
-
 	CGContextFillPath(contextBorder);
+
+	//Border
+	CGContextBeginPath(contextBorder);
+	CGContextMoveToPoint(contextBorder, currentSize.width - BORDER_OFFSET, currentSize.height);
+	CGContextAddLineToPoint(contextBorder, currentSize.width - BORDER_OFFSET, radius + BORDER_OFFSET);
+	CGContextAddArc(contextBorder, currentSize.width - radius - BORDER_OFFSET, radius + BORDER_OFFSET, radius, 0, -M_PI_2, 1);
+	CGContextAddLineToPoint(contextBorder, radius + BORDER_OFFSET, BORDER_OFFSET);
+	CGContextAddArc(contextBorder, radius + BORDER_OFFSET, radius + BORDER_OFFSET, radius, -M_PI_2, -M_PI, 1);
+	CGContextAddLineToPoint(contextBorder, BORDER_OFFSET, currentSize.height);
+	CGContextStrokePath(contextBorder);
 }
 
 @end
