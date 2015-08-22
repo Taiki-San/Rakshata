738eec501353f2bc091993ae882d1bfbba18d990
Taiki <taiki@rakshata.com>
2015-08-22 15:54:55 +0200

    Tweak ident prefs

--- C/Lecteur/lecteur_loading.c
+++ C/Lecteur/lecteur_loading.c
@@ -16,9 +16,9 @@
 bool reader_getNextReadableElement(PROJECT_DATA projectDB, bool isTome, uint *currentPosIntoStructure)
 {
 	uint maxValue = isTome ? projectDB.nombreTomesInstalled : projectDB.nombreChapitreInstalled;
-
+	
 	for((*currentPosIntoStructure)++;	*currentPosIntoStructure < maxValue
-										&& !checkReadable(projectDB, isTome, ACCESS_ID(isTome, projectDB.chapitresInstalled[*currentPosIntoStructure], projectDB.tomesInstalled[*currentPosIntoStructure].ID));		(*currentPosIntoStructure)++);
+		&& !checkReadable(projectDB, isTome, ACCESS_ID(isTome, projectDB.chapitresInstalled[*currentPosIntoStructure], projectDB.tomesInstalled[*currentPosIntoStructure].ID));		(*currentPosIntoStructure)++);
 	
 	return *currentPosIntoStructure < maxValue;
 }
@@ -31,10 +31,10 @@ bool configFileLoader(PROJECT_DATA projectDB, bool isTome, uint IDRequested, DAT
 	uint chapterRequestedForVolume = INVALID_VALUE;
 	char name[LONGUEUR_NOM_PAGE], input_path[LONGUEUR_NOM_PAGE], **nomPagesTmp = NULL, *encodedPath = getPathForProject(projectDB);
 	CONTENT_TOME *localBuffer = NULL;
-    void * intermediaryPtr;
+	void * intermediaryPtr;
 	
-    dataReader->nombrePage = 0;
-    dataReader->nomPages = dataReader->path = NULL;
+	dataReader->nombrePage = 0;
+	dataReader->nomPages = dataReader->path = NULL;
 	dataReader->pathNumber = NULL;
 	dataReader->chapitreTomeCPT = NULL;
 	dataReader->pageCouranteDuChapitre = NULL;
@@ -42,8 +42,8 @@ bool configFileLoader(PROJECT_DATA projectDB, bool isTome, uint IDRequested, DAT
 	if(encodedPath == NULL)
 		return false;
 	
-    if(isTome)
-    {
+	if(isTome)
+	{
 		uint pos;
 		for(pos = 0; pos < projectDB.nombreTomesInstalled && projectDB.tomesInstalled[pos].ID != IDRequested; pos++);
 		if(pos >= projectDB.nombreTomesInstalled)
@@ -55,11 +55,11 @@ bool configFileLoader(PROJECT_DATA projectDB, bool isTome, uint IDRequested, DAT
 		if(localBuffer == NULL)
 			return false;
 	}
-    else
+	else
 		nombreToursRequis = 1;
 	
-    for(uint nombreTours = 0; nombreTours < nombreToursRequis; nombreTours++)
-    {
+	for(uint nombreTours = 0; nombreTours < nombreToursRequis; nombreTours++)
+	{
 		if(isTome)
 		{
 			chapterRequestedForVolume = localBuffer[nombreTours].ID;
@@ -97,87 +97,87 @@ bool configFileLoader(PROJECT_DATA projectDB, bool isTome, uint IDRequested, DAT
 				snprintf(name, LONGUEUR_NOM_PAGE, CHAPTER_PREFIX"%u", IDRequested / 10);
 		}
 		
-        snprintf(input_path, LONGUEUR_NOM_PAGE, PROJECT_ROOT"%s/%s/%s", encodedPath, name, CONFIGFILE);
+		snprintf(input_path, LONGUEUR_NOM_PAGE, PROJECT_ROOT"%s/%s/%s", encodedPath, name, CONFIGFILE);
 		
-        nomPagesTmp = loadChapterConfigDat(input_path, &nombrePageInChunck);
-        if(nomPagesTmp != NULL)
-        {
-            /*On réalloue la mémoire en utilisant un buffer intermédiaire*/
-            dataReader->nombrePage += nombrePageInChunck;
+		nomPagesTmp = loadChapterConfigDat(input_path, &nombrePageInChunck);
+		if(nomPagesTmp != NULL)
+		{
+			/*On réalloue la mémoire en utilisant un buffer intermédiaire*/
+			dataReader->nombrePage += nombrePageInChunck;
 			
-            ///pathNumber
-            intermediaryPtr = realloc(dataReader->pathNumber, dataReader->nombrePage * sizeof(uint));
-            if(intermediaryPtr != NULL)
-                dataReader->pathNumber = intermediaryPtr;
-            else
-                goto memoryFail;
-			
-            ///pageCouranteDuChapitre
-            intermediaryPtr = realloc(dataReader->pageCouranteDuChapitre, (dataReader->nombrePage+1) * sizeof(uint));
-            if(intermediaryPtr != NULL)
-                dataReader->pageCouranteDuChapitre = intermediaryPtr;
-            else
-                goto memoryFail;
-			
-            ///nomPages
-            intermediaryPtr = realloc(dataReader->nomPages, dataReader->nombrePage * sizeof(char*));
-            if(intermediaryPtr != NULL)
-                dataReader->nomPages = intermediaryPtr;
-            else
-                goto memoryFail;
-			
-            ///chapitreTomeCPT
-            intermediaryPtr = realloc(dataReader->chapitreTomeCPT, (nombreTours + 2) * sizeof(uint));
-            if(intermediaryPtr != NULL)
-                dataReader->chapitreTomeCPT = intermediaryPtr;
-            else
-                goto memoryFail;
-			
-            ///path
-            intermediaryPtr = realloc(dataReader->path, (nombreTours + 2) * sizeof(char*));
-            if(intermediaryPtr != NULL)
-            {
-                dataReader->path = intermediaryPtr;
-                dataReader->path[nombreTours] = malloc(LONGUEUR_NOM_PAGE);
-
-                if(dataReader->path[nombreTours] == NULL)
-                    goto memoryFail;
-				
-				dataReader->path[nombreTours+1] = NULL;
-            }
-            else
+			///pathNumber
+			intermediaryPtr = realloc(dataReader->pathNumber, dataReader->nombrePage * sizeof(uint));
+			if(intermediaryPtr != NULL)
+				dataReader->pathNumber = intermediaryPtr;
+			else
 				goto memoryFail;
 			
-            if(0)  //Si on a eu un problème en allouant de la mémoire
-            {
-memoryFail:
+			///pageCouranteDuChapitre
+			intermediaryPtr = realloc(dataReader->pageCouranteDuChapitre, (dataReader->nombrePage+1) * sizeof(uint));
+			if(intermediaryPtr != NULL)
+				dataReader->pageCouranteDuChapitre = intermediaryPtr;
+			else
+				goto memoryFail;
+			
+			///nomPages
+			intermediaryPtr = realloc(dataReader->nomPages, dataReader->nombrePage * sizeof(char*));
+			if(intermediaryPtr != NULL)
+				dataReader->nomPages = intermediaryPtr;
+			else
+				goto memoryFail;
+			
+			///chapitreTomeCPT
+			intermediaryPtr = realloc(dataReader->chapitreTomeCPT, (nombreTours + 2) * sizeof(uint));
+			if(intermediaryPtr != NULL)
+				dataReader->chapitreTomeCPT = intermediaryPtr;
+			else
+				goto memoryFail;
+			
+			///path
+			intermediaryPtr = realloc(dataReader->path, (nombreTours + 2) * sizeof(char*));
+			if(intermediaryPtr != NULL)
+			{
+				dataReader->path = intermediaryPtr;
+				dataReader->path[nombreTours] = malloc(LONGUEUR_NOM_PAGE);
+				
+				if(dataReader->path[nombreTours] == NULL)
+					goto memoryFail;
+				
+				dataReader->path[nombreTours+1] = NULL;
+			}
+			else
+				goto memoryFail;
+			
+			if(0)  //Si on a eu un problème en allouant de la mémoire
+			{
+			memoryFail:
 				if(dataReader->pathNumber == NULL || dataReader->pageCouranteDuChapitre == NULL || dataReader->nomPages == NULL || dataReader->chapitreTomeCPT == NULL || dataReader->path == NULL || dataReader->path[nombreTours] == NULL)
 				{
 					free(dataReader->pathNumber);				dataReader->pathNumber = NULL;
 					free(dataReader->pageCouranteDuChapitre);	dataReader->pageCouranteDuChapitre = NULL;
 					free(dataReader->nomPages);					dataReader->nomPages = NULL;
 					free(dataReader->chapitreTomeCPT);			dataReader->chapitreTomeCPT = NULL;
-
+					
 					if(dataReader->path != NULL)
 						for(uint loop = 0; loop <= nombreTours; free(dataReader->path[loop++]));
 					
 					free(dataReader->path);						dataReader->path = NULL;
 				}
-                dataReader->nombrePage -= nombrePageInChunck;
-                nombreTours--;
-            }
-            else
-            {
-                snprintf(dataReader->path[posID], LONGUEUR_NOM_PAGE, PROJECT_ROOT"%s/%s", encodedPath, name);
-                if(isTome)
-                    dataReader->chapitreTomeCPT[posID] = chapterRequestedForVolume;
-                else
-                    dataReader->chapitreTomeCPT[posID] = IDRequested;
+				dataReader->nombrePage -= nombrePageInChunck;
+				nombreTours--;
+			}
+			else
+			{
+				snprintf(dataReader->path[posID], LONGUEUR_NOM_PAGE, PROJECT_ROOT"%s/%s", encodedPath, name);
+				if(isTome)
+					dataReader->chapitreTomeCPT[posID] = chapterRequestedForVolume;
+				else
+					dataReader->chapitreTomeCPT[posID] = IDRequested;
 				
-                lengthBasePath = strlen(dataReader->path[posID]);
+				lengthBasePath = strlen(dataReader->path[posID]);
 				
-                for(uint i = 0; prevPos < dataReader->nombrePage; prevPos++) //Réinintialisation
-                {
+				for(uint i = 0; prevPos < dataReader->nombrePage; prevPos++) //Réinintialisation
+				{
 					if(nomPagesTmp[i] == NULL)
 					{
 						prevPos--;
@@ -185,25 +185,25 @@ memoryFail:
 						continue;
 					}
 					
-                    lengthFullPath = lengthBasePath + strlen(nomPagesTmp[i]) + 0x10; // '/' + \0 + margin
-                    dataReader->nomPages[prevPos] = malloc(lengthFullPath);
-                    if(dataReader->nomPages[prevPos] != NULL)
-                    {
-                        snprintf(dataReader->nomPages[prevPos], lengthFullPath, "%s/%s", dataReader->path[posID], nomPagesTmp[i]);
-                        dataReader->pathNumber[prevPos] = posID;
-                        dataReader->pageCouranteDuChapitre[prevPos] = i++;
-                    }
-                    else    //Si problème d'allocation
-                        prevPos--;
-                }
-
+					lengthFullPath = lengthBasePath + strlen(nomPagesTmp[i]) + 0x10; // '/' + \0 + margin
+					dataReader->nomPages[prevPos] = malloc(lengthFullPath);
+					if(dataReader->nomPages[prevPos] != NULL)
+					{
+						snprintf(dataReader->nomPages[prevPos], lengthFullPath, "%s/%s", dataReader->path[posID], nomPagesTmp[i]);
+						dataReader->pathNumber[prevPos] = posID;
+						dataReader->pageCouranteDuChapitre[prevPos] = i++;
+					}
+					else    //Si problème d'allocation
+						prevPos--;
+				}
+				
 				posID++;
-
+				
 				for(uint i = 0; i < nombrePageInChunck; free(nomPagesTmp[i++]));
-            }
+			}
 			
 			free(nomPagesTmp);
-        }
+		}
 		else
 		{
 			nombreTours--;
@@ -211,25 +211,25 @@ memoryFail:
 		}
 	}
 	
-    if(dataReader->pathNumber != NULL && dataReader->nomPages != NULL)
-        dataReader->IDDisplayed = IDRequested;
-
+	if(dataReader->pathNumber != NULL && dataReader->nomPages != NULL)
+		dataReader->IDDisplayed = IDRequested;
+	
 	if(dataReader->pageCourante >= dataReader->nombrePage)
 		dataReader->pageCourante = dataReader->nombrePage != 0 ? dataReader->nombrePage - 1 : 0;
 	
 	free(encodedPath);
-    return true;
+	return true;
 }
 
 char ** loadChapterConfigDat(char* input, uint *nombrePage)
 {
-    char ** output, current;
-    FILE* fileInput = fopen(input, "r");
-
-	if(fileInput == NULL)
-        return NULL;
+	char ** output, current;
+	FILE* fileInput = fopen(input, "r");
 	
-    fscanf(fileInput, "%d", nombrePage);
+	if(fileInput == NULL)
+		return NULL;
+	
+	fscanf(fileInput, "%d", nombrePage);
 	
 	//We got to the next line
 	while((current = fgetc(fileInput)) != EOF)
@@ -239,21 +239,21 @@ char ** loadChapterConfigDat(char* input, uint *nombrePage)
 	}
 	
 	//We extract the data from the file
-    if(current != EOF)
-    {
+	if(current != EOF)
+	{
 		if(current != 'N')
 		{
 			fclose(fileInput);
 			logR("Obsolete "CONFIGFILE" detected, please notify the maintainer of the repository");
-
+			
 			return NULL;
 		}
 		
-        output = calloc(*nombrePage, sizeof(char*));
+		output = calloc(*nombrePage, sizeof(char*));
 		
-        for(uint i = 0, j; i < *nombrePage; i++)
-        {
-            output[i] = malloc(LONGUEUR_NOM_PAGE+1);
+		for(uint i = 0, j; i < *nombrePage; i++)
+		{
+			output[i] = malloc(LONGUEUR_NOM_PAGE+1);
 			if(output[i] == NULL)
 			{
 				memoryError(LONGUEUR_NOM_PAGE+1);
@@ -278,22 +278,22 @@ char ** loadChapterConfigDat(char* input, uint *nombrePage)
 				}
 				
 				*nombrePage = i;
-
+				
 				void * tmp = realloc(output, i * sizeof(char*));
 				if(tmp != NULL)
 					output = tmp;
 				
 				break;
 			}
-        }
-    }
-    else
-    {
-        output = malloc(++(*nombrePage) * sizeof(char*));
+		}
+	}
+	else
+	{
+		output = malloc(++(*nombrePage) * sizeof(char*));
 		
-        for(uint i = 0; i < *nombrePage; i++)
-        {
-            output[i] = malloc(20);
+		for(uint i = 0; i < *nombrePage; i++)
+		{
+			output[i] = malloc(20);
 			if(output[i] == NULL)
 			{
 				memoryError(20);
@@ -305,10 +305,10 @@ char ** loadChapterConfigDat(char* input, uint *nombrePage)
 			}
 			else
 				snprintf(output[i], 20, "%d.jpg", i);	//Sadly, legacy, use png as a default would have been more clever
-        }
-    }
+		}
+	}
 	
-    fclose(fileInput);
+	fclose(fileInput);
 	
 	//We remove config.dat from the path
 	for(uint i = strlen(input); i > 0 && input[i] != '/'; input[i--] = 0);
@@ -318,21 +318,21 @@ char ** loadChapterConfigDat(char* input, uint *nombrePage)
 	uint pathLength = strlen(input) + LONGUEUR_NOM_PAGE + 1;
 	char temp[pathLength];
 	
-    for(uint i = 0; i < *nombrePage; i++)
-    {
-        if(output[i] != NULL && output[i][0])
-        {
-            snprintf(temp, pathLength, "%s%s", input, output[i]);
-            if(!checkFileExist(temp))
-            {
+	for(uint i = 0; i < *nombrePage; i++)
+	{
+		if(output[i] != NULL && output[i][0])
+		{
+			snprintf(temp, pathLength, "%s%s", input, output[i]);
+			if(!checkFileExist(temp))
+			{
 				free(output[i]);
 				output[i] = NULL;
 				needCompact = true;
-            }
-        }
+			}
+		}
 		else
 			needCompact = true;
-    }
+	}
 	
 	//We compact the list if invalid items were detected
 	if(needCompact)
@@ -362,7 +362,7 @@ char ** loadChapterConfigDat(char* input, uint *nombrePage)
 		*nombrePage = validEntries;
 	}
 	
-    return output;
+	return output;
 }
 
 void releaseDataReader(DATA_LECTURE *data)
