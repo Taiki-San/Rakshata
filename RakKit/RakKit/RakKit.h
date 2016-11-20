bb0b0cf384bb2f580b93ad30ab47e5e123d928e0
Taiki <taiki@rakshata.com>
2016-11-20 16:53:13 +0100

    Update the new file with the licence header

--- RakKit/RakKit/RakKit.h
+++ RakKit/RakKit/RakKit.h
@@ -1,12 +1,18 @@
-//
-//  RakKit.h
-//  RakKit
-//
-//  Created by Taiki on 19/11/2016.
-//  Copyright © 2016 Taiki. All rights reserved.
-//
-
-#import <Cocoa/Cocoa.h>
+/*********************************************************************************************
+ **	__________         __           .__            __                 ________   _______   	**
+ **	\______   \_____  |  | __  _____|  |__ _____ _/  |______   	___  _\_____  \  \   _  \  	**
+ **	 |       _/\__  \ |  |/ / /  ___/  |  \\__  \\   __\__  \  	\  \/ //  ____/  /  /_\  \ 	**
+ **	 |    |   \ / __ \|    <  \___ \|   Y  \/ __ \|  |  / __ \_  \   //       \  \  \_/   \	**
+ **	 |____|_  /(____  /__|_ \/____  >___|  (____  /__| (____  /	  \_/ \_______ \ /\_____  /	**
+ **	        \/      \/     \/     \/     \/     \/          \/ 	              \/ \/     \/ 	**
+ **                                                                                         **
+ **			This Source Code Form is subject to the terms of the Mozilla Public				**
+ **			License, v. 2.0. If a copy of the MPL was not distributed with this				**
+ **			file, You can obtain one at https://mozilla.org/MPL/2.0/.						**
+ **                                                                                         **
+ **                     			© Taiki 2011-2016                                       **
+ **                                                                                         **
+ *********************************************************************************************/
 
 //! Project version number for RakKit.
 FOUNDATION_EXPORT double RakKitVersionNumber;
